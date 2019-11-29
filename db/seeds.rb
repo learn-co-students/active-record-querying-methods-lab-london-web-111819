45350b68efb5eea46d1b97684c68374805d6c080
badogan <basri.dogan.71@gmail.com>
2019-11-29 13:32:04 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10, :season=>43)
+Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7, :season=>42)
+Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2, :season=>42)
+Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3, :season=>41)
